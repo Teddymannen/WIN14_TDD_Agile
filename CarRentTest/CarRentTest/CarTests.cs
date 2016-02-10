0dd4b975401df1c5a125f23784f23b3140d2018f
Teddymannen <teddy_kross@hotmail.com>
2016-02-10 14:14:55 +0100

    changed Teddys test method name and made the car types global.

--- CarRentTest/CarRentTest/CarTests.cs
+++ CarRentTest/CarRentTest/CarTests.cs
@@ -3,23 +3,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CarRentTest
 {
+
     [TestClass]
     public class CarTests
     {
+        SportCar sportcar;
+        FamilyCar familycar;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sportcar = new SportCar();
+            familycar = new FamilyCar();
+        }
         [TestMethod]
         public void SportCarTest_Theo()
         {
-            SportCar sportcar = new SportCar();
             sportcar.ExtraInsurance = 150;
             Assert.IsTrue(sportcar.ExtraInsurance > 0);
         }
         [TestMethod]
-        public void TestCarMilesIsNegativeOrZero_Teddy()
+        public void TestCarMilesIsAboveZero_Teddy()
         {
-            SportCar sportcar = new SportCar();
             sportcar.MilesDriven = 50;
+            Assert.IsTrue(sportcar.MilesDriven > 0);
+            familycar.MilesDriven = 20;
+            Assert.IsTrue(familycar.MilesDriven > 0);
             
-            Assert.IsFalse(sportcar.MilesDriven >= 0);
         }
     }
 }
