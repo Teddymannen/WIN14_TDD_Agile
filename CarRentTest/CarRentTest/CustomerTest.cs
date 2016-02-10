6faa64bfda32b4a5d4cb33b0ef3705e5f8de931d
VirreX <Virrex@live.se>
2016-02-10 12:39:12 +0100

    CustomerTest.cs

---
+++ CarRentTest/CarRentTest/CustomerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarRentTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+    }
+}
