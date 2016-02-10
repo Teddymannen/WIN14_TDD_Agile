afc617dbf6bb42d5f2d591c5c20e7c91c00e4c58
Teddymannen <teddy_kross@hotmail.com>
2016-02-10 11:56:08 +0100

    mini-fix

--- CarRentTest/CarRentTest/UnitTest1.cs
+++ CarRentTest/CarRentTest/UnitTest1.cs
@@ -6,9 +6,10 @@ namespace CarRentTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void setup()
         {
+
         }
     }
 }
