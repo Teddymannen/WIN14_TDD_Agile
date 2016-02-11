d7f335b48dc2cde7edd5ebe4ac78d028b728a368
Theo Müller <theomuller@hotmail.com>
2016-02-11 13:22:07 +0100

    Confirmationruta klar, fin som fan

---
+++ CarRentTest/CarRent/Confirmation.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace CarRent
+{
+    partial class Confirmation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConfirmationLabel = new System.Windows.Forms.Label();
+            this.MileageLabel = new System.Windows.Forms.Label();
+            this.DailyFeeLabel = new System.Windows.Forms.Label();
+            this.InsuranceLabel = new System.Windows.Forms.Label();
+            this.DatesLabel = new System.Windows.Forms.Label();
+            this.CarTypeLabel = new System.Windows.Forms.Label();
+            this.FeesLabel = new System.Windows.Forms.Label();
+            this.InformationLabel = new System.Windows.Forms.Label();
+            this.Dates = new System.Windows.Forms.Label();
+            this.CarType = new System.Windows.Forms.Label();
+            this.MileageCost = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ExtraInsurance = new System.Windows.Forms.Label();
+            this.ConfirmBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConfirmationLabel
+            // 
+            this.ConfirmationLabel.AutoSize = true;
+            this.ConfirmationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConfirmationLabel.Location = new System.Drawing.Point(29, 9);
+            this.ConfirmationLabel.Name = "ConfirmationLabel";
+            this.ConfirmationLabel.Size = new System.Drawing.Size(273, 31);
+            this.ConfirmationLabel.TabIndex = 0;
+            this.ConfirmationLabel.Text = "Booking Confirmation";
+            // 
+            // MileageLabel
+            // 
+            this.MileageLabel.AutoSize = true;
+            this.MileageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MileageLabel.Location = new System.Drawing.Point(20, 193);
+            this.MileageLabel.Name = "MileageLabel";
+            this.MileageLabel.Size = new System.Drawing.Size(95, 20);
+            this.MileageLabel.TabIndex = 1;
+            this.MileageLabel.Text = "Mileage fee:";
+            // 
+            // DailyFeeLabel
+            // 
+            this.DailyFeeLabel.AutoSize = true;
+            this.DailyFeeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DailyFeeLabel.Location = new System.Drawing.Point(20, 218);
+            this.DailyFeeLabel.Name = "DailyFeeLabel";
+            this.DailyFeeLabel.Size = new System.Drawing.Size(74, 20);
+            this.DailyFeeLabel.TabIndex = 2;
+            this.DailyFeeLabel.Text = "Daily fee:";
+            // 
+            // InsuranceLabel
+            // 
+            this.InsuranceLabel.AutoSize = true;
+            this.InsuranceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InsuranceLabel.Location = new System.Drawing.Point(20, 243);
+            this.InsuranceLabel.Name = "InsuranceLabel";
+            this.InsuranceLabel.Size = new System.Drawing.Size(123, 20);
+            this.InsuranceLabel.TabIndex = 3;
+            this.InsuranceLabel.Text = "Extra insurance:";
+            // 
+            // DatesLabel
+            // 
+            this.DatesLabel.AutoSize = true;
+            this.DatesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DatesLabel.Location = new System.Drawing.Point(20, 94);
+            this.DatesLabel.Name = "DatesLabel";
+            this.DatesLabel.Size = new System.Drawing.Size(56, 20);
+            this.DatesLabel.TabIndex = 4;
+            this.DatesLabel.Text = "Dates:";
+            // 
+            // CarTypeLabel
+            // 
+            this.CarTypeLabel.AutoSize = true;
+            this.CarTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CarTypeLabel.Location = new System.Drawing.Point(20, 119);
+            this.CarTypeLabel.Name = "CarTypeLabel";
+            this.CarTypeLabel.Size = new System.Drawing.Size(72, 20);
+            this.CarTypeLabel.TabIndex = 5;
+            this.CarTypeLabel.Text = "Car type:";
+            // 
+            // FeesLabel
+            // 
+            this.FeesLabel.AutoSize = true;
+            this.FeesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FeesLabel.Location = new System.Drawing.Point(19, 163);
+            this.FeesLabel.Name = "FeesLabel";
+            this.FeesLabel.Size = new System.Drawing.Size(60, 25);
+            this.FeesLabel.TabIndex = 6;
+            this.FeesLabel.Text = "Fees";
+            // 
+            // InformationLabel
+            // 
+            this.InformationLabel.AutoSize = true;
+            this.InformationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InformationLabel.Location = new System.Drawing.Point(19, 63);
+            this.InformationLabel.Name = "InformationLabel";
+            this.InformationLabel.Size = new System.Drawing.Size(118, 25);
+            this.InformationLabel.TabIndex = 7;
+            this.InformationLabel.Text = "Information";
+            // 
+            // Dates
+            // 
+            this.Dates.AutoSize = true;
+            this.Dates.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dates.Location = new System.Drawing.Point(152, 97);
+            this.Dates.Name = "Dates";
+            this.Dates.Size = new System.Drawing.Size(146, 16);
+            this.Dates.TabIndex = 8;
+            this.Dates.Text = "2016-02-11 - 2016-12-20";
+            // 
+            // CarType
+            // 
+            this.CarType.AutoSize = true;
+            this.CarType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CarType.Location = new System.Drawing.Point(152, 122);
+            this.CarType.Name = "CarType";
+            this.CarType.Size = new System.Drawing.Size(62, 16);
+            this.CarType.TabIndex = 9;
+            this.CarType.Text = "Sport car";
+            // 
+            // MileageCost
+            // 
+            this.MileageCost.AutoSize = true;
+            this.MileageCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MileageCost.Location = new System.Drawing.Point(152, 196);
+            this.MileageCost.Name = "MileageCost";
+            this.MileageCost.Size = new System.Drawing.Size(57, 16);
+            this.MileageCost.TabIndex = 10;
+            this.MileageCost.Text = "2 kr / km";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(152, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "200 kr / day";
+            // 
+            // ExtraInsurance
+            // 
+            this.ExtraInsurance.AutoSize = true;
+            this.ExtraInsurance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExtraInsurance.Location = new System.Drawing.Point(152, 246);
+            this.ExtraInsurance.Name = "ExtraInsurance";
+            this.ExtraInsurance.Size = new System.Drawing.Size(43, 16);
+            this.ExtraInsurance.TabIndex = 12;
+            this.ExtraInsurance.Text = "150 kr";
+            // 
+            // ConfirmBtn
+            // 
+            this.ConfirmBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConfirmBtn.Location = new System.Drawing.Point(236, 331);
+            this.ConfirmBtn.Name = "ConfirmBtn";
+            this.ConfirmBtn.Size = new System.Drawing.Size(75, 39);
+            this.ConfirmBtn.TabIndex = 13;
+            this.ConfirmBtn.Text = "Confirm";
+            this.ConfirmBtn.UseVisualStyleBackColor = true;
+            this.ConfirmBtn.Click += new System.EventHandler(this.OkBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.Location = new System.Drawing.Point(155, 331);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 39);
+            this.CancelBtn.TabIndex = 14;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // Confirmation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(323, 382);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.ConfirmBtn);
+            this.Controls.Add(this.ExtraInsurance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MileageCost);
+            this.Controls.Add(this.CarType);
+            this.Controls.Add(this.Dates);
+            this.Controls.Add(this.InformationLabel);
+            this.Controls.Add(this.FeesLabel);
+            this.Controls.Add(this.CarTypeLabel);
+            this.Controls.Add(this.DatesLabel);
+            this.Controls.Add(this.InsuranceLabel);
+            this.Controls.Add(this.DailyFeeLabel);
+            this.Controls.Add(this.MileageLabel);
+            this.Controls.Add(this.ConfirmationLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Confirmation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Confirmation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ConfirmationLabel;
+        private System.Windows.Forms.Label MileageLabel;
+        private System.Windows.Forms.Label DailyFeeLabel;
+        private System.Windows.Forms.Label InsuranceLabel;
+        private System.Windows.Forms.Label DatesLabel;
+        private System.Windows.Forms.Label CarTypeLabel;
+        private System.Windows.Forms.Label FeesLabel;
+        private System.Windows.Forms.Label InformationLabel;
+        private System.Windows.Forms.Label Dates;
+        private System.Windows.Forms.Label CarType;
+        private System.Windows.Forms.Label MileageCost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ExtraInsurance;
+        private System.Windows.Forms.Button ConfirmBtn;
+        private System.Windows.Forms.Button CancelBtn;
+    }
+}
