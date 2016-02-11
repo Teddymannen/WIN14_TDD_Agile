c935f47a7f716f62b3a8c5fc776900c57f7f8c2d
Teddymannen <teddy_kross@hotmail.com>
2016-02-11 10:08:01 +0100

    bla

--- CarRentTest/CarRent/Classes/FamilyCar.cs
+++ CarRentTest/CarRent/Classes/FamilyCar.cs
@@ -11,16 +11,16 @@ using System.Text;
 
 public class FamilyCar : Car
 {
-    double value = 1000;
+    double miles = 1000;
     public double TotalMilesDriven
     {
         get
         {
-            return this.value;
+            return this.miles;
         }
         set
         {
-            this.value = value;
+            this.miles = value;
         }
     }
     public override double ExtraInsurance
