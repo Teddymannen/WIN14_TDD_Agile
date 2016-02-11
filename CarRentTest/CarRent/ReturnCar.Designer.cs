3277edc8dfc59e8d65094f9c8b13fc553b5534cf
Teddymannen <teddy_kross@hotmail.com>
2016-02-11 13:26:32 +0100

    Welcome(admin) page changed.

--- CarRentTest/CarRent/ReturnCar.Designer.cs
+++ CarRentTest/CarRent/ReturnCar.Designer.cs
@@ -28,24 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.welcomeAdmin = new System.Windows.Forms.Label();
             this.totalLabel = new System.Windows.Forms.Label();
             this.totalCostAmount = new System.Windows.Forms.Label();
-            this.milesDrivenAmount = new System.Windows.Forms.Label();
             this.milesDrivenLabel = new System.Windows.Forms.Label();
-            this.returnDateLabel = new System.Windows.Forms.Label();
+            this.actualReturnDateLabel = new System.Windows.Forms.Label();
             this.returnDate = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.carTypeLabel = new System.Windows.Forms.Label();
+            this.carType = new System.Windows.Forms.Label();
+            this.notesLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // welcomeAdmin
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label1.Location = new System.Drawing.Point(12, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome back";
+            this.welcomeAdmin.AutoSize = true;
+            this.welcomeAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeAdmin.Location = new System.Drawing.Point(6, 9);
+            this.welcomeAdmin.Name = "welcomeAdmin";
+            this.welcomeAdmin.Size = new System.Drawing.Size(224, 31);
+            this.welcomeAdmin.TabIndex = 0;
+            this.welcomeAdmin.Text = "Welcome (admin)";
             // 
             // totalLabel
             // 
@@ -61,41 +75,31 @@
             // 
             this.totalCostAmount.AutoSize = true;
             this.totalCostAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.totalCostAmount.Location = new System.Drawing.Point(78, 346);
+            this.totalCostAmount.Location = new System.Drawing.Point(125, 346);
             this.totalCostAmount.Name = "totalCostAmount";
             this.totalCostAmount.Size = new System.Drawing.Size(54, 20);
             this.totalCostAmount.TabIndex = 2;
             this.totalCostAmount.Text = "$4000";
             // 
-            // milesDrivenAmount
-            // 
-            this.milesDrivenAmount.AutoSize = true;
-            this.milesDrivenAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.milesDrivenAmount.Location = new System.Drawing.Point(109, 227);
-            this.milesDrivenAmount.Name = "milesDrivenAmount";
-            this.milesDrivenAmount.Size = new System.Drawing.Size(36, 20);
-            this.milesDrivenAmount.TabIndex = 4;
-            this.milesDrivenAmount.Text = "200";
-            // 
             // milesDrivenLabel
             // 
             this.milesDrivenLabel.AutoSize = true;
             this.milesDrivenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.milesDrivenLabel.Location = new System.Drawing.Point(12, 227);
+            this.milesDrivenLabel.Location = new System.Drawing.Point(13, 230);
             this.milesDrivenLabel.Name = "milesDrivenLabel";
             this.milesDrivenLabel.Size = new System.Drawing.Size(91, 20);
             this.milesDrivenLabel.TabIndex = 3;
             this.milesDrivenLabel.Text = "Miles driven";
             // 
-            // returnDateLabel
+            // actualReturnDateLabel
             // 
-            this.returnDateLabel.AutoSize = true;
-            this.returnDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.returnDateLabel.Location = new System.Drawing.Point(12, 173);
-            this.returnDateLabel.Name = "returnDateLabel";
-            this.returnDateLabel.Size = new System.Drawing.Size(97, 20);
-            this.returnDateLabel.TabIndex = 5;
-            this.returnDateLabel.Text = "Return Date";
+            this.actualReturnDateLabel.AutoSize = true;
+            this.actualReturnDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actualReturnDateLabel.Location = new System.Drawing.Point(13, 178);
+            this.actualReturnDateLabel.Name = "actualReturnDateLabel";
+            this.actualReturnDateLabel.Size = new System.Drawing.Size(97, 20);
+            this.actualReturnDateLabel.TabIndex = 5;
+            this.actualReturnDateLabel.Text = "Return Date";
             // 
             // returnDate
             // 
@@ -105,18 +109,168 @@
             this.returnDate.Size = new System.Drawing.Size(200, 26);
             this.returnDate.TabIndex = 7;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // carTypeLabel
+            // 
+            this.carTypeLabel.AutoSize = true;
+            this.carTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carTypeLabel.Location = new System.Drawing.Point(13, 107);
+            this.carTypeLabel.Name = "carTypeLabel";
+            this.carTypeLabel.Size = new System.Drawing.Size(68, 20);
+            this.carTypeLabel.TabIndex = 9;
+            this.carTypeLabel.Text = "Car type";
+            // 
+            // carType
+            // 
+            this.carType.AutoSize = true;
+            this.carType.Location = new System.Drawing.Point(128, 112);
+            this.carType.Name = "carType";
+            this.carType.Size = new System.Drawing.Size(62, 13);
+            this.carType.TabIndex = 10;
+            this.carType.Text = "placeholder";
+            // 
+            // notesLabel
+            // 
+            this.notesLabel.AutoSize = true;
+            this.notesLabel.Location = new System.Drawing.Point(129, 370);
+            this.notesLabel.Name = "notesLabel";
+            this.notesLabel.Size = new System.Drawing.Size(39, 13);
+            this.notesLabel.TabIndex = 11;
+            this.notesLabel.Text = "(notes)";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(12, 369);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(65, 27);
+            this.calcButton.TabIndex = 12;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 310);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Penalty";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Days booked";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(129, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "placeholder";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Total daily cost";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Actual days";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(128, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "placeholder";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(129, 263);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "days * cost-per-day";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 286);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Miles cost";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(129, 286);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "miles * cost-per-mile";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(129, 310);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(120, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "extradays * cost-per-day";
+            // 
             // ReturnCar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(638, 457);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.notesLabel);
+            this.Controls.Add(this.carType);
+            this.Controls.Add(this.carTypeLabel);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.returnDate);
-            this.Controls.Add(this.returnDateLabel);
-            this.Controls.Add(this.milesDrivenAmount);
+            this.Controls.Add(this.actualReturnDateLabel);
             this.Controls.Add(this.milesDrivenLabel);
             this.Controls.Add(this.totalCostAmount);
             this.Controls.Add(this.totalLabel);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.welcomeAdmin);
             this.Name = "ReturnCar";
             this.Text = "ReturnCar";
             this.Load += new System.EventHandler(this.ReturnCar_Load);
@@ -127,12 +281,26 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label welcomeAdmin;
         private System.Windows.Forms.Label totalLabel;
         private System.Windows.Forms.Label totalCostAmount;
-        private System.Windows.Forms.Label milesDrivenAmount;
         private System.Windows.Forms.Label milesDrivenLabel;
-        private System.Windows.Forms.Label returnDateLabel;
+        private System.Windows.Forms.Label actualReturnDateLabel;
         private System.Windows.Forms.DateTimePicker returnDate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label carTypeLabel;
+        private System.Windows.Forms.Label carType;
+        private System.Windows.Forms.Label notesLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
